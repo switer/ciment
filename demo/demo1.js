be102d4277d83b8d93ba23171ed3428170ba6f5e
switer <guankaishe@gmail.com>
2013-10-06 10:47:14 +0800

    add file

---
+++ demo/demo1.js
@@ -0,0 +1,12 @@
+var comment = require('../comment');
+
+console.log('\n--------------- comment module methods -------------------\n');
+console.log(comment.single('hello world'));
+console.log(comment.block('hello world'));
+console.log(comment.title('hello world'));
+console.log(comment.banner('hello world'));
+console.log('\n\n------------ comment prototype methods ----------------\n');
+console.log('hello world'.single());
+console.log('hello world'.block());
+console.log('hello world'.title());
+console.log('hello world'.banner());
